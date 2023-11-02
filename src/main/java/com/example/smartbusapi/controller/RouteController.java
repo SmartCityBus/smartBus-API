7bcd4f1b3387adada7d3e6d47e8b7630703d98ca
chilyoon <nby5699@gmail.com>
2023-11-02 16:50:48 +0900

    Route API 대구, 경산 데이터 저장

--- src/main/java/com/example/smartbusapi/controller/RouteController.java
+++ src/main/java/com/example/smartbusapi/controller/RouteController.java
@@ -13,6 +13,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
@@ -55,56 +56,62 @@ public class RouteController {
 
     // 노선 API 요청 함수
     private void getRouteData(Route route) throws InterruptedException, ExecutionException, IOException {
-        StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1613000/ArvlInfoInqireService/getSttnAcctoArvlPrearngeInfoList"); /*URL*/
-        urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "=" + serviceKey); /*Service Key*/
-        urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지번호*/
-        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("10", "UTF-8")); /*한 페이지 결과 수*/
-        urlBuilder.append("&" + URLEncoder.encode("_type","UTF-8") + "=" + URLEncoder.encode("json", "UTF-8")); /*데이터 타입(xml, json)*/
-        urlBuilder.append("&" + URLEncoder.encode("cityCode","UTF-8") + "=" + URLEncoder.encode("37100", "UTF-8")); /*도시코드 [상세기능3 도시코드 목록 조회]에서 조회 가능*/
-        // 대구대삼거리 정류장 nodeId
-        urlBuilder.append("&" + URLEncoder.encode("nodeId","UTF-8") + "=" + URLEncoder.encode("GYB360008200", "UTF-8")); /*정류소ID [국토교통부(TAGO)_버스정류소정보]에서 조회가능*/
-        URL url = new URL(urlBuilder.toString());
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setRequestMethod("GET");
-        conn.setRequestProperty("Content-type", "application/json");
-        System.out.println("Response code: " + conn.getResponseCode());
-        BufferedReader rd;
-        if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
-            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-        } else {
-            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
-        }
-        StringBuilder sb = new StringBuilder();
-        String line;
-        while ((line = rd.readLine()) != null) {
-            sb.append(line);
-        }
-        rd.close();
-        conn.disconnect();
+        // 같은 정류장이지만 도시별로 요청해야함
+        List<List<String>> reqDatas = Arrays.asList(Arrays.asList("22","DGB7121008400"), Arrays.asList("37100", "GYB360008400"));
 
-        ObjectMapper objectMapper = new ObjectMapper();
-        JsonNode rootNode = objectMapper.readTree(sb.toString());
-        JsonNode response = rootNode.get("response");
-        System.out.println("response : " + response);
-        if (response != null) {
-            JsonNode body = response.get("body");
-            System.out.println("body : " + body);
-            if (body != null) {
-                JsonNode items = body.get("items");
-                System.out.println("items : " + items);
-                if (items != null) {
-                    // "item" 객체 가져오기
-                    JsonNode item = items.get("item");
-                    System.out.println("item : " + item);
-                    // 검색 결과가 단일 결과, 다중 결과로 나오기 때문에 조건에 따라 처리
-                    if (item != null) {
-                        if (item.isArray() && item.size() > 0){
-                            for (int i = 0; i < item.size(); i++) {
-                                JsonNode curItem = item.get(i);
-                                processVehicle(curItem, route);
+        for (List<String> reqData : reqDatas) {
+            StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1613000/ArvlInfoInqireService/getSttnAcctoArvlPrearngeInfoList"); /*URL*/
+            urlBuilder.append("?" + URLEncoder.encode("serviceKey", "UTF-8") + "=" + serviceKey); /*Service Key*/
+            urlBuilder.append("&" + URLEncoder.encode("pageNo", "UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지번호*/
+            urlBuilder.append("&" + URLEncoder.encode("numOfRows", "UTF-8") + "=" + URLEncoder.encode("10", "UTF-8")); /*한 페이지 결과 수*/
+            urlBuilder.append("&" + URLEncoder.encode("_type", "UTF-8") + "=" + URLEncoder.encode("json", "UTF-8")); /*데이터 타입(xml, json)*/
+            urlBuilder.append("&" + URLEncoder.encode("cityCode", "UTF-8") + "=" + URLEncoder.encode(reqData.get(0), "UTF-8")); /*도시코드 [상세기능3 도시코드 목록 조회]에서 조회 가능*/
+            urlBuilder.append("&" + URLEncoder.encode("nodeId", "UTF-8") + "=" + URLEncoder.encode(reqData.get(1), "UTF-8")); /*정류소ID [국토교통부(TAGO)_버스정류소정보]에서 조회가능*/
+            URL url = new URL(urlBuilder.toString());
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Content-type", "application/json");
+            System.out.println("Response code: " + conn.getResponseCode());
+            BufferedReader rd;
+            if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+                rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            } else {
+                rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+            }
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = rd.readLine()) != null) {
+                sb.append(line);
+            }
+            rd.close();
+            conn.disconnect();
+
+            System.out.println("도시 코드 : " + reqData.get(0));
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode rootNode = objectMapper.readTree(sb.toString());
+            JsonNode response = rootNode.get("response");
+            System.out.println("response : " + response);
+            if (response != null) {
+                JsonNode body = response.get("body");
+                System.out.println("body : " + body);
+                if (body != null) {
+                    JsonNode items = body.get("items");
+                    System.out.println("items : " + items);
+                    if (items != null) {
+                        // "item" 객체 가져오기
+                        JsonNode item = items.get("item");
+                        System.out.println("item : " + item);
+                        // 검색 결과가 단일 결과, 다중 결과로 나오기 때문에 조건에 따라 처리
+                        if (item != null) {
+                            if (item.isArray() && item.size() > 0) {
+                                for (int i = 0; i < item.size(); i++) {
+                                    JsonNode curItem = item.get(i);
+                                    processVehicle(curItem, route);
+                                }
+                            } else {
+                                processVehicle(item, route);
                             }
-                        } else{
-                            processVehicle(item, route);
                         }
                     }
                 }
@@ -116,7 +123,7 @@ public class RouteController {
     private void processVehicle (JsonNode item, Route route) throws ExecutionException, InterruptedException {
         JsonNode routeidNode = item.get("routeid");
         JsonNode routenoNode = item.get("routeno");
-        System.out.println(routeidNode + " " + routenoNode);
+//        System.out.println(routeidNode + " " + routenoNode);
         if (routeidNode != null && routenoNode != null) {
             String routeid = routeidNode.asText();
             String routeno = routenoNode.asText();
