754d6a476bea66d17312f6059b6c214c63fa048a
chilyoon <nby5699@gmail.com>
2023-11-03 15:26:46 +0900

    Route, Vehicle 스케줄러 생성

---
+++ src/main/java/com/example/smartbusapi/dataSchedule.java
@@ -0,0 +1,41 @@
+package com.example.smartbusapi;
+
+import com.example.smartbusapi.controller.RouteController;
+import com.example.smartbusapi.controller.VehicleController;
+import com.example.smartbusapi.model.Route;
+import com.example.smartbusapi.model.Vehicle;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class dataSchedule {
+
+    private final RouteController routeController;
+    private final VehicleController vehicleController;
+
+    public dataSchedule(RouteController routeController, VehicleController vehicleController) {
+        this.routeController = routeController;
+        this.vehicleController = vehicleController;
+    }
+
+    // 일정 주기마다 route, vehicle 최신화 하는 함수
+    @Scheduled(fixedDelay = 60000) // 10분
+    public void scheduled_route() {
+        try {
+            System.out.println("route 스케줄러 실행");
+            routeController.createStation(new Route());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Scheduled(fixedDelay = 60000, initialDelay = 6000) // 10분, 초기 딜레이 1분
+    public void scheduled_vehicle() {
+        try {
+            System.out.println("vehicle 스케줄러 실행");
+            vehicleController.createVehicle(new Vehicle());
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
